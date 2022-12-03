ba630bf8cccaaa66a114724322145e7ab9b5bbfb
bettyblue <daghoster@gmail.com>
2022-12-04 01:30:42 +0200

    * Enable mousewheel scrolling in tmux * Add undotree plugin. Still need to understand how it works

--- nvim/.config/nvim/after/plugin/packer.lua
+++ nvim/.config/nvim/after/plugin/packer.lua
@@ -1,30 +1,34 @@
-require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+require("packer").startup(function(use)
+    use "wbthomason/packer.nvim"
 
     -- status line
     use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true }
     }
 
     -- telescope
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { 'nvim-lua/plenary.nvim' }
+        "nvim-telescope/telescope.nvim", tag = "0.1.0",
+        requires = { "nvim-lua/plenary.nvim" }
     }
 
     -- treesitter
     use {
-        'nvim-treesitter/nvim-treesitter',
-        run = { ':TSUpdate' }
+        "nvim-treesitter/nvim-treesitter",
+        run = { ":TSUpdate" }
     }
 
     -- LSP
-    use 'neovim/nvim-lspconfig' 
+    use "neovim/nvim-lspconfig" 
 
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
+    -- auto completion
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-nvim-lsp"
+
+    -- undo tree
+    use "mbbill/undotree"
 
     -- theme
-    use 'folke/tokyonight.nvim'
+    use "folke/tokyonight.nvim"
 end)
